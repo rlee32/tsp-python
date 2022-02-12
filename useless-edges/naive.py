a96aefb680964977c527df938ab076fce042520b
Robert Lee <rlee32@pm.me>
2022-02-12 04:10:35 -0500

    finish naive useless-edge finder

---
+++ useless-edges/naive.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+# Calculates 'useless edges' using a naive algorithm.
+# A 'useless edge' is an edge that is not part of any improving or neutral 2-opt move.
+
+from typing import Dict, Tuple, List
+from tsp_reader import read_instance
+from tsp_math import distance
+
+def is_useless_edge(instance: Dict[int, Tuple[float, float]], a: int, b: int) -> bool:
+    """Returns True if the input edge specified by 2 endpoint IDs (a, b) is useless. """
+    all_indices = list(instance.keys())
+    ab = distance(instance=instance, a=a, b=b)
+    for c in all_indices:
+        if c in (a, b):
+            continue
+        c_useless = True
+        for d in all_indices:
+            if d in (a, b, c):
+                continue
+            cd = distance(instance=instance, a=c, b=d)
+            ac = distance(instance=instance, a=a, b=c)
+            bd = distance(instance=instance, a=b, b=d)
+            if ab + cd < ac + bd:
+                c_useless = False
+                break
+            ad = distance(instance=instance, a=a, b=d)
+            bc = distance(instance=instance, a=b, b=c)
+            if ab + cd < ad + bc:
+                c_useless = False
+                break
+        if c_useless:
+            return True
+    return False
+
+def get_non_useless_edges(instance: Dict[int, Tuple[float, float]]) -> List[Tuple[int, int]]:
+    all_indices = list(instance.keys())
+    n = len(all_indices)
+    non_useless_edges = []
+    for i in range(n):
+        print(i)
+        a = all_indices[i]
+        for j in range(i + 1, n):
+            b = all_indices[j]
+            if is_useless_edge(instance=instance, a=a, b=b):
+                continue
+            non_useless_edges.append((a, b))
+    return non_useless_edges
+
+def get_total_edge_count(instance: Dict[int, Tuple[float, float]]) -> int:
+    n = len(instance)
+    return int(n * (n - 1) / 2)
+
+import sys
+
+if __name__ == "__main__":
+    # Read instance file.
+    if len(sys.argv) > 1:
+        instance_path = sys.argv[1]
+        print(f"Reading instance file at path: {instance_path}")
+        instance = read_instance(path=instance_path)
+        print(f"Read {len(instance)} points in instance.")
+        total_edge_count = get_total_edge_count(instance=instance)
+        print(f"Total edge count: {total_edge_count}")
+        non_useless_edges = get_non_useless_edges(instance)
+        print(f"Non useless edge count: {len(non_useless_edges)}")
+
--- useless-edges/tsp_math.py
+++ useless-edges/tsp_math.py
@@ -2,15 +2,15 @@
 
 from typing import Dict, Tuple
 
-def distance(instance: Dict[int, Tuple[float, float]], i: int, j: int) -> int:
+def distance(instance: Dict[int, Tuple[float, float]], a: int, b: int) -> int:
     """instance is a dict of point IDs to x, y coordinates.
-    i and j are the point IDs of the points we want to calculate the distance between.
+    a and b are the point IDs of the points we want to calculate the distance between.
     Returns distance as rounded int, as per TSPLIB standard.
     """
-    ix = instance[i][0]
-    iy = instance[i][1]
-    jx = instance[j][0]
-    jy = instance[j][1]
-    dx = jx - ix
-    dy = jy - iy
+    ax = instance[a][0]
+    ay = instance[a][1]
+    bx = instance[b][0]
+    by = instance[b][1]
+    dx = bx - ax
+    dy = by - ay
     return int(round((dx ** 2 + dy ** 2) ** 0.5))
