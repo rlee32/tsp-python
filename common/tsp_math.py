c57d48acd874f3e46254fd6634048280fed2b090
Robert Lee <rlee32@pm.me>
2022-02-18 04:36:22 -0500

    add two functions for tour differencing

--- common/tsp_math.py
+++ common/tsp_math.py
@@ -77,3 +77,46 @@ def add_midpoints_to_instance(instance: Instance, edges: List[Edge]):
     for edge in edges:
         new_point_ids.append(add_midpoint_to_instance(instance=instance, edge=edge))
     return new_point_ids
+
+def _normalize_edge(edge: Edge) -> Edge:
+    a, b = edge
+    return (min(a, b), max(a, b))
+
+def _normalize_edges(edges: List[Edge]) -> List[Edge]:
+    return [normalize_edge(edge) for edge in edges]
+
+def tour_difference(old_tour: Tour, new_tour: Tour) -> Tuple[List[Edge], List[Edge]]:
+    old_edges = set(_normalize_edges(get_edges_from_tour(old_tour)))
+    new_edges = set(_normalize_edges(get_edges_from_tour(new_tour)))
+    deleted_edges = []
+    for old_edge in old_edges:
+        if old_edge not in new_edges:
+            deleted_edges.append(old_edge)
+    added_edges = []
+    for new_edge in new_edges:
+        if new_edge not in old_edges:
+            added_edges.append(new_edge)
+    assert(len(deleted_edges) == len(added_edges))
+    return deleted_edges, added_edges
+
+def disjoin_edges(edges: List[Edge]):
+    point_sets = {0: set()}
+    edge_sets = {0: []}
+    for edge in edges:
+        a, b = edge
+        added = False
+        for set_id in point_sets:
+            point_set = point_sets[set_id]
+            if a in point_set or b in point_set:
+                point_sets[set_id].add(a)
+                point_sets[set_id].add(b)
+                edge_sets[set_id].append(edge)
+                added = True
+                break
+        if not added:
+            new_set_id = len(point_sets)
+            point_sets[new_set_id] = set()
+            point_sets[new_set_id].add(a)
+            point_sets[new_set_id].add(b)
+            edge_sets[new_set_id] = [edge]
+    return list(edge_sets.values())
